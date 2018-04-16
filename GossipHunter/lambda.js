cdb8341dcca2f5ba88aa9cd3dd473c53e27d6cf7
Janaka Bandara <janakaud@gmail.com>
2018-04-16 11:03:21 +0530

    added DDB table

--- GossipHunter/lambda.js
+++ GossipHunter/lambda.js
@@ -1,11 +1,28 @@
 let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
 let request = require('request');
 
-exports.handler = function(event, context, callback) {
-	request.get(`https://newsapi.org/v2/top-headlines?sources=entertainment-weekly&apiKey=${process.env.KEY}`,
-    (error, response, body) => {
-console.log(body);
+exports.handler = function (event, context, callback) {
+    request.get(`https://newsapi.org/v2/top-headlines?sources=entertainment-weekly&apiKey=${process.env.KEY}`,
+        (error, response, body) => {
+            let result = JSON.parse(body);
+            if (result.status !== "ok") {
+                return callback('NewsAPI call failed!');
+            }
+            result.articles.forEach(article => {
+                ddb.get({
+                    TableName: 'gossips',
+                    Key: { 'url': article.url }
+                }, function (err, data) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log(data);
+                    }
+                });
 
-        callback(null,'Successfully executed');
-    })
+            });
+
+            callback(null, 'Successfully executed');
+        })
 }

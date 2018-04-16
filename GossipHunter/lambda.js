fb2541b2e7efde5cf6c7009ab9b1112b0c3b4ee6
Janaka Bandara <janakaud@gmail.com>
2018-04-16 09:51:31 +0530

    restarted

--- GossipHunter/lambda.js
+++ GossipHunter/lambda.js
@@ -1,10 +1,8 @@
 let AWS = require('aws-sdk');
-let requests = require('requests');
-
 exports.handler = function(event, context, callback) {
-	requests.get("https://newsapi.org/v2/top-headlines?sources=entertainment-weekly&apiKey=")
-	.then(response => {
-console.log(response);
-		callback(null,'Successfully executed');
-	})
+	requests.get(`https://newsapi.org/v2/top-headlines?sources=entertainment-weekly&apiKey=${process.env.KEY}`)
+		.then(response => {
+
+			callback(null,'Successfully executed');
+		})
 }
